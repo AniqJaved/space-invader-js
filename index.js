cd7b401d5ed92cc42f43b795907df433529ec314
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-11-18 18:45:54 +0500

    invader (enemy created)

--- index.js
+++ index.js
@@ -93,7 +93,58 @@ class Projectile {
     }
 }
 
+
+class Invader { 
+    constructor(){ 
+        
+        this.velocity = { 
+            x: 0, 
+            y: 0 
+        } 
+        
+        
+
+        //getting image
+
+        const image = new Image()
+        image.src = './images/enemy.png'
+
+        image.onload = () =>{        //This function runs only when the image is completely loaded
+            const scale = 0.03
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale
+            this.position = { 
+                x: canvas.width / 2 - this.width / 2,  // we are using this.width so the area covered by image itself is also distributed evenly
+                y: canvas.height / 2 
+            } 
+        }
+    } 
+
+    //Apperance of our player
+    draw(){
+        //Just making a box
+        // c.fillStyle = 'red'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+
+        
+
+        //Using the spaceship image 
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(){
+        if(this.image){  //Runs only when the image is loaded.
+            this.draw()
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+        } 
+    }
+}
+
 const player = new Player()
+const invader = new Invader()
 const projectiles = [
     
 ]
@@ -114,6 +165,7 @@ function animate(){
     requestAnimationFrame(animate) //we are using this request animation frame because it will be running after every few milliseconds and if we dont use it then the image that we are extracting will not load. But in this case as it is reloading then the image gets load.
     c.fillStyle = 'black'
     c.fillRect(0,0,canvas.width,canvas.height)
+    invader.update()
     player.update()
     projectiles.forEach( (projectile, index) => {  //Here we are using update method on every projectile to make a bullet of it.
         if(projectile.position.y + projectile.radius <= 0){  //Removing the projectile from the array when they are out of the canvas.
